bad81087a17ef2eac68904775f6cbbb482446f21
MasonMeirs <maseymoose@gmail.com>
2020-10-15 04:11:22 -0700

    Working first version (#1)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-const { spawn } = require("child_process");
+const { exec } = require("child_process");
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
-  });
+  })
+  
+
 mainShellLoop = () => {
-    readline.question('_>', input => {
-        var splitInput = input.split(' ');
-        console.dir(splitInput);
-        spawn(splitInput[0], splitInput);
+    readline.question('_>', (result) => {
+        exec(result, (error, stdout, stderr) =>{
+            if(error) {
+                console.log('BlazeShell: error: ${error.message}');
+                mainShellLoop();
+            }
+            if (stderr) {
+                console.log(`${stderr}`);
+                mainShellLoop();
+            }
+            console.log(`${stdout}`);
+            mainShellLoop();
+        });   
     });
 }
 mainShellLoop();
